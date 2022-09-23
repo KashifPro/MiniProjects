faed4ae4aea1622d777d2d3d7559bccb9e1c1ee6
Kashif <kas@gmail.com>
2022-09-23 13:12:26 +0500

    Header nav Tab

---
+++ Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav>
+        <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>Contact</li>
+        <li>Projects</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Header
